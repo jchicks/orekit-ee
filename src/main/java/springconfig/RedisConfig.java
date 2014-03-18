42eb3335998b493025c4163018fb65c3afb5b63b
Jerry <jerry.hicks@flightsystems.net>
2014-03-18 17:32:32 -0400

    convert tabs to spaces

--- src/main/java/springconfig/RedisConfig.java
+++ src/main/java/springconfig/RedisConfig.java
@@ -8,55 +8,56 @@ import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-//springframework.data.redis.serializer.StringRedisSerializer;
+
 
 @Configuration("RedisConfig")
 public class RedisConfig 
 {
-	public static final String CHANNEL_PREFIX = "orekit::operation::";
-	
-	@Bean(name="jedisConnectionFactory")
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
+  public static final String CHANNEL_PREFIX = "orekit::operation::";
 
-    @Bean
-    RedisTemplate< String, Object > redisTemplate() {
-        final RedisTemplate< String, Object > template =  new RedisTemplate< String, Object >();
-        template.setConnectionFactory( jedisConnectionFactory() );
-        template.setKeySerializer( new StringRedisSerializer() );
-        template.setHashValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-        template.setValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
-        return template;
-    }
+  @Bean(name="jedisConnectionFactory")
+  JedisConnectionFactory jedisConnectionFactory() {
+    return new JedisConnectionFactory();
+  }
 
-//    @Bean
-//    MessageListenerAdapter messageListener() {
-//        return new MessageListenerAdapter( 
-//        	new RedisMessageListener() );
-//    }
+  @Bean
+  RedisTemplate< String, Object > redisTemplate() {
+    final RedisTemplate< String, Object > template =  new RedisTemplate< String, Object >();
+    template.setConnectionFactory( jedisConnectionFactory() );
+    template.setKeySerializer( new StringRedisSerializer() );
+    template.setHashValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
+    template.setValueSerializer( new GenericToStringSerializer< Object >( Object.class ) );
+    return template;
+  }
 
-    @Bean(name="redisContainer")
-    RedisMessageListenerContainer redisContainer() {
-		System.out.println("==========");
-		System.out.println("seting up listener container");
-		System.out.println("==========");
-		
-        final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+  //    @Bean
+  //    MessageListenerAdapter messageListener() {
+  //        return new MessageListenerAdapter( 
+  //        	new RedisMessageListener() );
+  //    }
 
-        container.setConnectionFactory( jedisConnectionFactory() );
-        //container.addMessageListener( messageListener(), orekitTopic() );
+  @Bean(name="redisContainer")
+  RedisMessageListenerContainer redisContainer() {
+    System.out.println("==========");
+    System.out.println("seting up listener container");
+    System.out.println("==========");
 
-        return container;
-    }
- 
-//    @Bean
-//    IRedisPublisher redisPublisher() {
-//        return new RedisPublisherImpl( redisTemplate(), topic() );
-//    }
+    final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 
-    @Bean(name="orekitTopic")
-    PatternTopic orekitTopic() {
-        return new PatternTopic( "pubsub:queue*" );
-    }
+    container.setConnectionFactory( jedisConnectionFactory() );
+    //container.addMessageListener( messageListener(), orekitTopic() );
+
+    return container;
+  }
+
+  //    @Bean
+  //    IRedisPublisher redisPublisher() {
+  //        return new RedisPublisherImpl( redisTemplate(), topic() );
+  //    }
+
+  @Bean(name="orekitTopic")
+  PatternTopic orekitTopic() 
+  {
+    return new PatternTopic( "pubsub:queue*" );
+  }
 }
